afdfa5e04e41de339632ef3c61d9894eef5131af
mazyaniv <mazya@post.bgu.ac.il>
2025-08-13 13:01:02 +0300

    commits

--- magazine_2.5tau.py
+++ magazine_2.5tau.py
@@ -8,14 +8,14 @@ import math
 chosen_space = np.linspace(-7.5, 5, 20) #dB
 sigma_space = 10**(-chosen_space/10)
 plot_result = 1
-save_to_mat = 0
+save_to_mat = 1
 list_output = []
 na,nq = 1,100
 bound_sim = 500
 thresh = 2
 matrix_const0 = Matrix(na, 0)
 matrix_const1 = Matrix(na, nq)
-plot_dict = {'LMMSE': 1, 'MMSE': 1 ,'Approx': 1, 'OPT':1,'WBCRB': 1, 'BCRB': 1}
+plot_dict = {'LMMSE': 0, 'MMSE': 0 ,'Approx': 1, 'OPT':1,'WBCRB': 0, 'BCRB': 0}
 
 if plot_dict['LMMSE'] == 1:
     LMMSE = np.squeeze([MSE_general_numerical(sigma_space[i], sigma_space[i], na, nq,matrix_const1,20000,thresh,thresh) for i in range(len(chosen_space))])
